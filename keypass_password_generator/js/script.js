c9b968f6c2f39bb47601886078a978188a4e4fd9
mahmoudzakaria12 <mahmoudzsali247@gmail.com>
2023-06-05 16:50:23 +0300

    add keypass file

---
+++ keypass_password_generator/js/script.js
@@ -0,0 +1,61 @@
+let lengthSlider = document.querySelector('.pass-length input'),
+    options = document.querySelectorAll('.options .option input'),
+    passInput = document.querySelector('.input-box input'),
+    passIndicator = document.querySelector('.pass-indicator'),
+    copyBtn = document.querySelector('.input-box i'),
+    generatePasswordBtn = document.querySelector('button.generate-btn');
+
+let characters = {
+    lowercase: 'abcdefghijklmnopqrstuvwxyz',
+    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    numbers: '0123456789',
+    symbols: '!@#$%^&*()_+-=<>/|~;:.,',
+}
+
+let generatePassword = _ => {
+    let staticPassword = '',
+        randomPassword = '',
+        excludeDuplicate = false,
+        passLength = lengthSlider.value;
+    options.forEach(option => {
+        if (option.checked) {
+            if (option.id !== 'spaces' && option.id !== 'exc-duplicate') {
+                staticPassword += characters[option.id];
+            } else if (option.id === 'spaces') {
+                staticPassword += `  ${staticPassword}  `;
+            } else {
+                excludeDuplicate = true;
+            }
+        }
+    });
+    for (let i = 0; i < passLength; i++) {
+        // get random character from the static password
+        let randomChar = staticPassword[Math.floor(Math.random() * staticPassword.length)];
+        if (excludeDuplicate) {  // if exludeDuplicate is true
+            !randomPassword.includes(randomChar) || randomChar == " " ? randomPassword += randomChar : i--;
+        } else {
+            randomPassword += randomChar;
+        }
+    }
+    passInput.value = randomPassword;
+};
+let updatePassIndicator = _ => {
+    passIndicator.id = lengthSlider.value <= 8 ? 'weak' : lengthSlider.value <= 16 ? "medium" : "strong";
+}
+let updateSlider = _ => {
+    document.querySelector('.pass-length .detials span').innerText = lengthSlider.value;
+    generatePassword();
+    updatePassIndicator();
+};
+let copyPassword = _ => {
+    if (copyBtn.classList.contains('fa-copy')) {
+        copyBtn.classList.replace('fa-copy', 'fa-check');
+        navigator.clipboard.writeText(passInput.value);
+    }
+    setTimeout(_ => copyBtn.classList.replace('fa-check', 'fa-copy'), 1500);
+}
+updateSlider();
+
+lengthSlider.addEventListener('input', updateSlider);
+generatePasswordBtn.addEventListener('click', generatePassword);
+copyBtn.addEventListener('click', copyPassword);
