885b496a4512e92774e94a1161c44f2f850c9cbe
mahmoudzakaria12 <mahmoudzsali247@gmail.com>
2023-07-22 16:15:45 +0300

    Update uploading btn

--- up_vue_video_player/js/script.js
+++ up_vue_video_player/js/script.js
@@ -13,15 +13,30 @@ let container = document.querySelector('.container'),
     picInPicBtn = document.querySelector('.pic-in-pic span'),
     fullSecreenBtn = document.querySelector('.fullscreen span'),
     currentVideoTime = document.querySelector('.current-time'),
-    videoDuration = document.querySelector('.video-duration');
+    videoDuration = document.querySelector('.video-duration'),
+    uploadBtn = document.querySelector('.upload-vid span'),
+    videoFileInput = document.getElementById('movie-file-input'),
+    layout = document.querySelector('.layout');
 
 let timer;
 
+function setVideo(src) {
+    mainVideo.src = src;
+    container.classList.add('show-controls')
+    mainVideo.addEventListener('loadedmetadata', _ => {
+        videoDuration.innerText = formatTime(mainVideo.duration);
+        progressBar.style.width = `0%`
+        playPauseBtn.innerText = 'play_arrow';
+        layout.classList.remove('active');
+        console.log("Hi");
+        mainVideo.pause();
+    });
+}
 
-let hideControls = _ => {
+function hideControls() {
     if (mainVideo.paused) return;
     timer = setTimeout(_ => container.classList.remove('show-controls'), 3000)
-};
+}
 
 hideControls();
 
@@ -30,7 +45,8 @@ container.addEventListener('mousemove', _ => {
     clearTimeout(timer);
     hideControls();
 });
-let formatTime = time => {
+
+function formatTime(time) {
     let seconds = Math.floor(time % 60),
         minutes = Math.floor(time / 60) % 60,
         hours = Math.floor(time / 3600);
@@ -48,12 +64,13 @@ setTimeout(_ => {
     console.log('ready');
 }, 300);
 
-let draggableProgressBar = e => {
+function draggableProgressBar(e) {
     let timeLineWidth = videoTimeLine.clientWidth;
     progressBar.style.width = `${e.offsetX}px`;
     mainVideo.currentTime = (e.offsetX / timeLineWidth) * mainVideo.duration;
     currentVideoTime.innerText = formatTime(mainVideo.currentTime);
 };
+
 videoTimeLine.addEventListener('mousedown', _ => {
     videoTimeLine.addEventListener('mousemove', draggableProgressBar);
 });
@@ -66,11 +83,13 @@ videoTimeLine.addEventListener('click', e => {
     let timeLineWidth = videoTimeLine.clientWidth;
     mainVideo.currentTime = (e.offsetX / timeLineWidth) * mainVideo.duration;
 });
+
 fullSecreenBtn.addEventListener('click', _ => {
     container.classList.toggle('fullscreen');
     if (document.fullscreenElement) {
         console.log("Not");
         picInPicBtn.parentElement.style.display = 'flex';
+        uploadBtn.parentElement.style.display = 'flex';
         document.querySelector('.video-controls').style = "margin-bottom: 7px;"
         // fullSecreenBtn.classList.replace('fa-compress', 'fa-expand');
         fullSecreenBtn.innerText = 'fullscreen';
@@ -82,6 +101,7 @@ fullSecreenBtn.addEventListener('click', _ => {
     document.querySelector('.wrapper').classList.add('at-full');
     document.querySelector('.video-controls').style = "margin-bottom: 0px;"
     picInPicBtn.parentElement.style.display = 'none';
+    uploadBtn.parentElement.style.display = 'none';
     // fullSecreenBtn.classList.replace('fa-expand', 'fa-compress');
     fullSecreenBtn.innerText = 'fullscreen_exit';
     container.requestFullscreen();
@@ -143,6 +163,7 @@ volumeInput.addEventListener('input', _ => {
 });
 skipBackward.addEventListener('click', _ => mainVideo.currentTime -= 5);
 skipForward.addEventListener('click', _ => mainVideo.currentTime += 10);
+
 playPauseBtn.addEventListener('click', _ => {
     if (mainVideo.paused) {
         mainVideo.play();
@@ -170,4 +191,21 @@ document.addEventListener('keyup', e => {
         hideControls();
     } else if (e.key == 'ArrowRight') skipForward.click();
     else if (e.key == 'ArrowLeft') skipBackward.click();
-})
+});
+
+uploadBtn.addEventListener('click', _ => videoFileInput.click());
+
+videoFileInput.addEventListener('input', _ => {
+    let file = videoFileInput.files[0];
+    if (file && (file.type == 'video/webm' || file.type == 'video/mp4')) {
+        layout.classList.add('active');
+        let reader = new FileReader();
+        reader.onload = _ => {
+            let url = reader.result;
+            setVideo(url);
+            videoFileInput.value = '';
+        }
+        reader.readAsDataURL(file);
+    }
+});
+
